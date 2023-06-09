152ed72d2db6a6f09561492134ce43d1b44bcbc9
AdolfoPastelin <adolfo.pe97@gmail.com>
2023-06-08 21:54:08 -0600

    Add functional sliding tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useState, useEffect } from 'react'
+
+const tabs = [
+  {
+    id: 'home',
+    name: 'Home'
+  },
+  {
+    id: 'projects',
+    name: 'Projects'
+  },
+  {
+    id: 'contact',
+    name: 'Contact'
+  },
+  {
+    id: 'blog',
+    name: 'Blog'
+  }
+]
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
+  const tabsRef = useRef<Array<HTMLElement | null>>([])
+  const [activeTabIndex, setActiveTabIndex] = useState<number | null>(null)
+  const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0)
+  const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0)
+
+  useEffect(() => {
+    if (activeTabIndex === null) return
+
+    const setTabPosition = (): void => {
+      const currentTab = tabsRef.current[activeTabIndex] as HTMLElement
+      setTabUnderlineWidth(currentTab?.clientWidth ?? 0)
+      setTabUnderlineLeft(currentTab?.offsetLeft ?? 0)
+    }
+
+    setTabPosition()
+  }, [activeTabIndex])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => { setCount((count) => count + 1) }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2 className='text-2xl text-center text-yellow-400 py-10'>Animated Sliding Tabs</h2>
+
+      <section className='flex relative h-12 rounded-3xl border border-black/40 bg-neutral-800 gap-8 px-4 backdrop-blur-sm'>
+        <span
+          className='flex absolute bottom-0 top-0 -z-10 overflow-hidden rounded-3xl py-2 transition-all duration-300'
+          style={{ left: tabUnderlineLeft, width: tabUnderlineWidth }}
+        >
+          <span className="h-full w-full rounded-3xl bg-yellow-400/90"></span>
+        </span>
+
+        {tabs.map((tab, index) => {
+          const isActive = activeTabIndex === index
+
+          return (
+            <button
+              key={index}
+              ref={(element) => (tabsRef.current[index] = element)}
+              className={`${isActive ? 'text-slate-900' : 'hover:text-neutral-200'
+                } 
+              my-auto cursor-pointer select-none rounded-full px-4 text-center text-white`}
+              onClick={() => { setActiveTabIndex(index) }}
+            >
+              {tab.name}
+            </button>
+          )
+        })}
+      </section>
     </>
   )
 }
