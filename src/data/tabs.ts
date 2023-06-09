ae29dabee754673e31357995a1d391f236a94986
AdolfoPastelin <adolfo.pe97@gmail.com>
2023-06-09 00:37:37 -0600

    Separate tabs into data own file

---
+++ src/data/tabs.ts
@@ -0,0 +1,18 @@
+export const tabs = [
+  {
+    id: 'home',
+    name: 'Home'
+  },
+  {
+    id: 'projects',
+    name: 'Projects'
+  },
+  {
+    id: 'contact',
+    name: 'Contact'
+  },
+  {
+    id: 'blog',
+    name: 'Blog'
+  }
+]
