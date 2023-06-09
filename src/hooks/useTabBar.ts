5e96a28a9891354120103a03702ffe1ee232971c
AdolfoPastelin <adolfo.pe97@gmail.com>
2023-06-09 00:37:59 -0600

    Add tab bar reusable custom hook

---
+++ src/hooks/useTabBar.ts
@@ -0,0 +1,30 @@
+import { useRef, useState, useEffect } from 'react'
+
+interface TabBar {
+  tabsRef: React.MutableRefObject<Array<HTMLElement | null>>
+  activeTabIndex: number | null
+  tabUnderlineWidth: number
+  tabUnderlineLeft: number
+  setActiveTabIndex: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export const useTabBar = (): TabBar => {
+  const tabsRef = useRef<Array<HTMLElement | null>>([])
+  const [activeTabIndex, setActiveTabIndex] = useState<number | null>(null)
+  const [tabUnderlineWidth, setTabUnderlineWidth] = useState(0)
+  const [tabUnderlineLeft, setTabUnderlineLeft] = useState(0)
+
+  useEffect(() => {
+    if (activeTabIndex === null) return
+
+    const setTabPosition = (): void => {
+      const currentTab = tabsRef.current[activeTabIndex] as HTMLElement
+      setTabUnderlineWidth(currentTab?.clientWidth ?? 0)
+      setTabUnderlineLeft(currentTab?.offsetLeft ?? 0)
+    }
+
+    setTabPosition()
+  }, [activeTabIndex])
+
+  return { tabsRef, activeTabIndex, tabUnderlineWidth, tabUnderlineLeft, setActiveTabIndex }
+}
